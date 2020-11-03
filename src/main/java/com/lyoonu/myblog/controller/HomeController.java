2019572b137c0751f4b2780bb1535de127675ada
Lyoonu <lyoonu17@gmail.com>
2020-11-04 00:02:41 +0900

    layout

---
+++ src/main/java/com/lyoonu/myblog/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.lyoonu.myblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+}
