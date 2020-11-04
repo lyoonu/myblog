3fe84ac6036f869eef9a89859f017737a44ff6c0
Lyoonu <lyoonu17@gmail.com>
2020-11-04 21:01:37 +0900

    Pagination

--- src/main/java/com/lyoonu/myblog/controller/BoardController.java
+++ src/main/java/com/lyoonu/myblog/controller/BoardController.java
@@ -1,10 +1,11 @@
 package com.lyoonu.myblog.controller;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -28,9 +29,14 @@ public class BoardController {
 	BoardValidator boardValidator;
 	
 	@GetMapping("/list")
-	public String list(Model model) {
+	public String list(Model model, @PageableDefault(size = 2) Pageable pageable) {
 		
-		List<Board> boards = boardRepository.findAll();
+		Page<Board> boards = boardRepository.findAll(pageable);
+		// boards.getTotalElements(); 타임리프 th:text="${boards.totalElements}" 에서 바로 실행가능
+		int startPage = Math.max(1,boards.getPageable().getPageNumber() - 4);
+		int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
 		model.addAttribute("boards", boards);
 		
 		return "board/list";
